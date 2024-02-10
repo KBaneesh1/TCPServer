1a9f73498f5dfbf7d7c06504f192504026086705
ANEESH K B <kbaneesh2002@gmail.com>
2024-02-10 08:58:46 +0530

    queue

--- src/parallel/main.cpp
+++ src/parallel/main.cpp
@@ -3,13 +3,14 @@
 #include <cstring>
 #include <cstdlib>
 #include <unistd.h>
+#include<bits/stdc++.h>
 #include<map>
 #include <pthread.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include<sstream>
 #include<typeinfo>
-
+#define NUM_THREADS 10
 
 using namespace std;
 
@@ -19,36 +20,39 @@ int acceptConnection(int serverSd);
 void *handleClient(void *p_socket);
 void closeConnection(int serverSd, int clientSd);
 
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+// data structure initialisation
+
+pthread_mutex_t map_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t queue_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t req_lock = PTHREAD_MUTEX_INITIALIZER;
 map<string,string>mp;
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
-        cerr << "Usage: " << argv[0] << " <port>" << endl;
-        exit(EXIT_FAILURE);
-    }
+queue<pthread_t>req; // put the request to queue , then handle it
+queue<int>empty_pos; //used to schedule 
+pthread_t arr[NUM_THREADS];
 
-    int port = atoi(argv[1]);
-    int serverSd = initializeServer(port);
 
-    
-    while (true)
-    {
-        int clientSd = acceptConnection(serverSd);
-        pthread_t t;
+void *scheduler(){
+    while(true){
+        try{
         int *pclient = (int *)malloc(sizeof(int));
-        *pclient = clientSd;
-        pthread_create(&t, NULL, &handleClient, pclient);
-        // handleClient(clientSd);
-        //closeConnection(serverSd, clientSd);
+        // looping until request queue is not empty 
+        while(req.empty() || empty_pos.empty());
+        pthread_mutex_lock(&req_lock);
+        *pclient = req.front();
+        req.pop();
+        pthread_mutex_unlock(&req_lock);    
+        pthread_create(&arr[empty_pos.front()], NULL, &handleClient, pclient);
+        }
+        catch(const char *errormsg){
+            cerr<<"Error : "<<errormsg<<endl;
+            break;
+        }
     }
-     close(serverSd);
-    printf("Server socket finished!!");
-
-    return 0;
+    pthread_exit(NULL);
+    return NULL;
 }
+
 
 int initializeServer(int port)
 {
@@ -101,7 +105,17 @@ void *handleClient(void *p_socket)
 {
     int clientSd = *((int *)p_socket);
     cout<<"client socket="<<clientSd<<endl;
+
+    // the empty position is taken by the thread
+    // the req is being handled , so it is popped out
+    int used_pos = -1;
     
+    pthread_mutex_lock(&queue_lock);
+    if(!empty_pos.empty())
+        used_pos =empty_pos.front();
+        empty_pos.pop();
+    pthread_mutex_unlock(&queue_lock);
+
     int bytesRead = 0, bytesWritten = 0;
     char buffer[4096];
     cout << "Awaiting client response..." << endl;
@@ -113,7 +127,7 @@ void *handleClient(void *p_socket)
             cerr << "Error reading from client!" << endl;
            return NULL;
         }
-    cout<<buffer<<endl;
+    // cout<<buffer<<endl;
     while(true){
         // Split the received data into lines
         string receivedData(buffer);
@@ -128,7 +142,12 @@ void *handleClient(void *p_socket)
             {
                 cout << "Client has quit the session" << endl;
                 close(clientSd);
-        	pthread_exit(NULL);
+                pthread_mutex_lock(&queue_lock);
+                if(used_pos!=-1)
+                    empty_pos.push(used_pos);
+                pthread_mutex_unlock(&queue_lock);
+
+        	    pthread_exit(NULL);
                 return NULL;
             }
 
@@ -140,11 +159,11 @@ void *handleClient(void *p_socket)
                 string key,value;
                 getline(ss,key,'\n');
                 getline(ss,value,'\n');
-                pthread_mutex_lock(&lock);
+                pthread_mutex_lock(&map_lock);
                 if(value[0]==':')
                     value.erase(0,1);
                 mp[key] = value;
-                pthread_mutex_unlock(&lock);
+                pthread_mutex_unlock(&map_lock);
                 store = "FIN\n";
                 // break;
             }
@@ -182,9 +201,16 @@ void *handleClient(void *p_socket)
             // Send the response to the client
             bytesWritten = send(clientSd, store.c_str(), store.size(), 0);
         }	
-      }
-        close(clientSd);
-        return NULL;
+    }
+    close(clientSd);
+    
+    // after all the operations are done add the used arr position to empty arr
+    pthread_mutex_lock(&queue_lock);
+    if(used_pos!=-1)
+        empty_pos.push(used_pos);
+    pthread_mutex_unlock(&queue_lock);
+
+    return NULL;
 
     // cout << " Bytes read: " << bytesRead << endl;
 }
@@ -194,4 +220,39 @@ void closeConnection(int serverSd, int clientSd)
     close(clientSd);
     cout << "Connection closed..." << endl;
 }
+ 
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        cerr << "Usage: " << argv[0] << " <port>" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    int port = atoi(argv[1]);
+    int serverSd = initializeServer(port);
+    
+    // initializing empty queue
+    for(int i=0;i<NUM_THREADS;i++){
+        empty_pos.push(i);
+    }
+    pthread_t t;
+    pthread_create(&t,NULL,&scheduler);
+    while (true)
+    {
+        int clientSd = acceptConnection(serverSd);
+
+        // for pushing the request and checking empty
+        pthread_mutex_lock(&req_lock);
+        req.push(clientSd);
+        pthread_mutex_lock(&req_lock);
+        // handleClient(clientSd);
+        //closeConnection(serverSd, clientSd);
+    }
+    close(serverSd);
+    printf("Server socket finished!!");
+
+    return 0;
+}
+
 
