058c55bb7f99fc279c77a3888c871bb30cbdb951
ANEESH K B <kbaneesh2002@gmail.com>
2024-02-11 14:22:00 +0530

    siuu

--- test.sh
+++ test.sh
@@ -26,8 +26,8 @@ fi
 
 START=$(($(date +%s%N)/1000000))
 
-rm tests/inputs_generated/*.txt
-rm tests/outputs/*.txt
+rm -p tests/inputs_generated/*.txt
+rm -p tests/outputs/*.txt
 
 mkdir -p tests/inputs_generated
 mkdir -p tests/outputs
